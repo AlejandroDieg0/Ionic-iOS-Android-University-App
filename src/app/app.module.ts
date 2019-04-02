868b69f0391a3997a7db4b15e273949f722c32bb
AlejandroDieg0 <alessandrocascino@outlook.it>
2019-04-02 19:06:07 +0200

    parsing json in listview con click

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,15 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { OneSignal } from '@ionic-native/onesignal/ngx';
+import {HttpClientModule} from "@angular/common/http";
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
+      HttpClientModule,
     IonicModule.forRoot(),
     AppRoutingModule
   ],
--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from "@ionic/angular";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-list',
@@ -6,41 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['list.page.scss']
 })
 export class ListPage implements OnInit {
-  private selectedItem: any;
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
-  ];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
-  constructor() {
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+
+  public items:any;
+  constructor(public navCtrl: NavController, public http: HttpClient){
+  this.loadData();
   }
 
+  loadData(){
+
+
+    let data:Observable<any>;
+    data = this.http.get("https://jsonplaceholder.typicode.com/posts");
+    data.subscribe(result=> {
+      this.items=result;
+    })
+  }
+  itemClick(itemid:number){
+    alert(itemid);
+  }
   ngOnInit() {
 
-    window['plugins'].OneSignal.sendTag("a", "b");
+    window['plugins'].OneSignal.sendTag("key", "value");
 
     window['plugins'].OneSignal.getTags(function(tags) {
       console.log('Tags Received: ' + JSON.stringify(tags));
+
     });
   }
-
-  // add back when alpha.4 is out
-  // navigate(item) {
-  //   this.router.navigate(['/list', JSON.stringify(item)]);
-  // }
 }
