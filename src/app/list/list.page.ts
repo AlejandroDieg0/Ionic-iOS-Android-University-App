8a78ae0ee354c6b6ee3bc139407e649fe3a929a5
Roberto <disantoroberto@gmail.com>
2019-04-03 17:54:31 +0200

    esperimenti senza risultati

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import { ALLOW_MULTIPLE_PLATFORMS } from '@angular/core/src/application_ref';
 
 @Component({
   selector: 'app-list',
@@ -22,16 +23,44 @@ export class ListPage implements OnInit {
       this.items = result;
     });
   }
+
   itemClick(item: number) { // passo item come dizionario, cosi ho tutti gli attributi
     alert('Ti sei iscritto agli avvisi del professor ' +
             item['nome'] + ' ' + item['cognome'] + '\nID:' + item['id'] );
     window['plugins'].OneSignal.sendTag(item['id'], 1);
   }
-  ngOnInit() {
 
+  ngOnInit() {
     window['plugins'].OneSignal.getTags(function(tags) {
       console.log('Tags Received: ' + JSON.stringify(tags));
-
+      // alert('Tags Received: ' + JSON.stringify(tags)); // mostra un avviso con i tag, utile per debug
     });
   }
 }
+
+/*
+esperimenti di merda per i gettags
+
+in gettags
+alert('test2' + JSON.parse(tags) ); // NON funziona
+alert('test3' + tags[0] );
+this.items = [ 1, 2, 3, 4, 5, 6 ]; // non funziona, presumo che non trovi "this"
+
+esperimenti per rinominare i json, inutili
+  loadData_daitag() { // scarica i tag e li mette nell'elenco
+    // this.items = [1, 2, 3 ];
+    // funziona
+    // this.items = JSON.parse('[{"cognome" : "c","id" : "1","nome" : "n"},{"cognome" : "f","id" : "2","nome" : "N"}]');
+    let stringa = '{"33" : "AA", "44" : "BB" }';
+    this.items = JSON.parse(stringa);
+    stringa = stringa.replace('{', '[');
+    stringa = stringa.replace('}', ']');
+    alert(
+      JSON.parse('["33" : "AA", "44" : "BB"]')
+      // stringa
+    );
+    this.items[0] = 33;
+    this.items[1] = 33;
+  }
+
+*/
