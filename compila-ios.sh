c6fbeeb198ded6de8b3736074964bda97b35092d
Roberto <disantoroberto@gmail.com>
2020-02-22 14:40:02 +0100

    aggiunte info per ionic 6

--- compila-ios.sh
+++ compila-ios.sh
@@ -8,6 +8,11 @@
 # ionic cordova platform add ios@latest --save
 # ionic cordova plugin remove onesignal-cordova-plugin
 # ionic cordova plugin add onesignal-cordova-plugin
+# cordova plugin rm cordova-plugin-ionic-webview
+# cordova plugin add cordova-plugin-ionic-webview@latest
 set -e
 ionic cordova build ios --device -- --buildFlag="-UseModernBuildSystem=0"
 ios-deploy -b platforms/ios/build/device/DocentiPUSH.ipa
+
+# testflight
+# ionic cordova build ios --prod
